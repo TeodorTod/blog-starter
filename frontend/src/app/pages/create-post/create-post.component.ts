4c2ea0e2955fef64f8ce9401ac8de885a6c836fa
TeodorTod <teodor.at.tod@gmail.com>
2024-03-07 23:04:20 +0200

    commit

---
+++ frontend/src/app/pages/create-post/create-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-post.component.html',
+})
+
+export class CreatePostComponent {
+
+}
