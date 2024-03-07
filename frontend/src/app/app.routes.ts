7c0b8e9fc2affbcea37720b9840a90c26ee81a26
TeodorTod <teodor.at.tod@gmail.com>
2024-03-07 22:45:08 +0200

    commit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)
+    },
+    {
+        path: 'create-post', loadComponent: () => import('./pages/create-post/create-post.component').then((c) => c.CreatePostComponent)
+    },
+    {
+        path: 'edit-post/:id', loadComponent: () => import('./pages/edit-post/edit-post.component').then((c) => c.EditPostComponent)
+    },
+]
